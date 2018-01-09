bde0a8f028e5c94bb452600510e48b607cea39c5
Unknown <peerawas.a@gmail.com>
2018-01-10 00:31:50 +0800

    secondMap

--- gmap_sim.sh
+++ gmap_sim.sh
@@ -1,2 +1,2 @@
-rosrun gmapping slam_gmapping scan:=/r1/front_laser/scan _odom_frame:=/r1/odom _base_frame:=/r1/base_link
+rosrun gmapping slam_gmapping scan:=/r1/front_laser/scan _odom_frame:=/r1/odom _base_frame:=/r1/base_link _particles:=100 _linearUpdate:=0.1 _angularUpdate:=0.03 _xmax:=10.0 _ymax:=10.0
 
