fa558ec7544b1ad14e2436de490beabcb7e19f4d
dchenam <dnnschen39@gmail.com>
2018-01-11 06:59:03 +0800

    insta shutdown

--- person/person.py
+++ person/person.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int8
+from darknet_ros_msgs.msg import BoundingBoxes
+from std_msgs.msg import Int8, String
 from actionlib_msgs.msg import GoalID
+import roslaunch
 
 class RecognitionNode(object):
     def __init__(self):
         rospy.init_node('recognition_node')
-        rospy.Subscriber("/darknet_ros/found_object", Int8, self.person_callback, queue_size=10)
+        rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, self.person_callback, queue_size=10)
         self.stop_pub = rospy.Publisher("/move_base/cancel", GoalID, queue_size=10)
-        self.stop_pub = rospy.Publisher("/move_base/cancel", GoalID, queue_size=10)
+        self.cmd_pub = rospy.Publisher("/command", String, queue_size=10)
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+        self.launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/dchenam/catkin_ws/launch/tracking.launch"])
+        self.launch.start()
+
     def person_callback(self, msg):
-        if msg.data == 2:
+        if msg.boundingBoxes[0].Class == "person":
             stop_msg = GoalID()
             stop_msg.id = ''
-            self.
+            voice_stop = String()
+            voice_stop.data = 'Please Provide Your Identification Card for Checking'
+            self.cmd_pub.publish(voice_stop)
             self.stop_pub.publish(stop_msg)
+            self.launch.shutdown()
 if __name__ == "__main__":
     recognition_node = RecognitionNode()
     rospy.spin()
